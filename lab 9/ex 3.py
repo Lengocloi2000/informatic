c20e94ba0f94ece685756c1c1e602c14f96b6619
Lengocloi2000 <90633603+Lengocloi2000@users.noreply.github.com>
2021-11-01 21:37:58 +0300

    Update ex 3.py

--- lab 9/ex 3.py
+++ lab 9/ex 3.py
@@ -6,49 +6,29 @@
  приводит его к нижнему регистру и убирает знаки препинания, при итерировании возвращаются нормализованные тексты, аналогично методу __getitem__.
 '''
 
-import zipfile
-import pathlib
-import re
-import requests
-import io
+import string
 import os
 
 class TextLoader:
     def __init__(self, address):
-        r = requests.get(address)
-        with r, zipfile.ZipFile(io.BytesIO(r.content)) as zip_file:
-            zip_file.extractall('file')
-        self.path = pathlib.Path('sample')
-        list_files  =  [name for name in list(self.path.glob('**/*.txt')) if name.is_file()]
-        self.files = list_files
-        self.iterable = iter(self.files)
+        self.path = address
+        list_files  =  os.listdir(self.path)
+        self.files = [address + '/' + list_files[k] for k,file in enumerate(list_files)]
 
     def __len__(self):
         return len(self.files)
 
-    def __next__(self):
-        text = next(self.iterable)
-        with text.open('r') as f:
-            r = f.read()
-        with text.open('w') as f:
-            norm = self.norma_text(r)
-            f.write(norm)
-
-    def __iter__(self):
-        return self
-
-    def norma_text(self,t):
-         t = re.sub(r'[,.:;?!]','', t)
-         t = t.lower()
+    def __getitem__(self, x):
+        arr = []
+        file = open(self.files[x], "r", encoding='utf-8')
+        for line in file:
+            line = line.lower()
+            arr.append(line.translate(str.maketrans('', '', string.punctuation)))
+        file.close()
+        return arr
 
 if __name__ == "__main__":
-    address = 'https://github.com/Lengocloi2000/informatic/blob/main/sample.zip'
+    address = 'D:\PycharmProjects\dino pygame\lap trinh huong doi tuong\lab 9\sample'
     a = TextLoader(address)
     print(len(a))
-    for i in range(len(a)):
-        try:
-            b = next(a)
-            print(b.read()[:20])
-            print("----------")
-        except StopIteration:
-            print("End.")
+    print(*a[3])
