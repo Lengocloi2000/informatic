871caf13a88651199f2e4aa097b2375140d250ff
Lengocloi2000 <90633603+Lengocloi2000@users.noreply.github.com>
2021-11-29 22:37:33 +0300

    Update ex 3.py

--- lab 13/ex 3.py
+++ lab 13/ex 3.py
@@ -1,38 +1,70 @@
-import string
+import pickle
 import os
+import string
 
 class TextLoader:
-    def __init__(self, address):
-        self.path = address
-        list_files  =  os.listdir(self.path)
-        self.files = [address + '/' + list_files[k] for k,file in enumerate(list_files)]
-        self.file = open(self.files[3], encoding='utf-8')
+    def __init__ (self, adress):
+        self.path = adress
+        self.files_list = [x for x in list(os.listdir(self.path))]
+        self.iterable = iter (self.files_list)
+        self.made = set()
 
-    def __len__(self):
-        return len(self.files)
+    def __len__ (self):
+        return len(self.files_list)
 
-    def __getitem__(self, x):
-        arr = []
-        file = open(self.files[x], "r", encoding='utf-8')
+    def __getitem__(self, path):
+        return formatted(path)
+
+    def formatted (self, path):
+        file = open(path, "r+t", encoding='utf-8')
+        text = ''
         for line in file:
             line = line.lower()
-            arr.append(line.translate(str.maketrans('', '', string.punctuation)))
+            text += line.translate(str.maketrans('', '', string.punctuation))
         file.close()
-        return arr
+        return text
+
+    def __iter__ (self):
+         return self
+
+    def __next__ (self):
+        file_path = next(self.iterable)
+        while file_path in self.made:
+            file_path = next(self.iterable)
+        self.made.add(file_path)
+        print (file_path)
+        text = self.formatted(file_path)
+        return text
 
     def __getstate__(self):
         state = self.__dict__.copy()
-        del state['file']
         return state
+
+    def ndump(self):
+        os.chdir('..')
+        with open('./data.pickle', 'wb') as f:
+            pickle.dump(self, f , pickle.HIGHEST_PROTOCOL)
 
     def __setstate__(self, state):
         self.__dict__.update(state)
-        file = open(self.file)
-        print(*self.__getitem__(3))
-        self.file = file
 
-if __name__ == "__main__":
-    address = 'D:\PycharmProjects\dino pygame\lap trinh huong doi tuong\lab 9\sample'
-    a = TextLoader(address)
-    print(len(a))
-    print(*a[3])
+
+if not os.path.exists('./data.pickle'):
+    texts = TextLoader('sample')
+else:
+    with open("./data.pickle", "rb") as f:
+        texts = pickle.load(f)
+        texts.files_list = [x for x in list(os.listdir(texts.path))]
+        texts.iterable = iter (texts.files_list)
+os.chdir('sample')
+while True:
+    print("command: ", end= '')
+    inp = input()
+    if inp == 'next':
+        try:
+            print(texts.__next__())
+        except Exception:
+            break
+    if inp == 'end':
+        texts.ndump()
+        break
